24c18e5309b07567e435ee6033143a9876cda362
ilia <jamiashvili@gmail.com>
2020-08-29 11:29:56 +0600

    Created models Poll and Choice, registered in admin.py, migrated, added test data, created fixture

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from webapp.models import Poll, Choice
 
-# Register your models here.
+# superuser
+# login: admin
+# password: admin
+admin.site.register(Poll)
+admin.site.register(Choice)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    text = models.CharField(max_length=200, verbose_name='Poll text')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+
+    def __str__(self):
+        return self.text
+
+
+class Choice(models.Model):
+    text = models.TextField(max_length=1000, verbose_name='Choice text')
+    poll = models.ForeignKey('webapp.Poll', on_delete=models.CASCADE,
+                             verbose_name='Poll FK')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+
+    def __str__(self):
+        return self.text
